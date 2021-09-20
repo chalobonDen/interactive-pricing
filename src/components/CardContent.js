b4edbe3f7040063d9f1610c7f48f9ba23dd6a76d
chalobon <chalobon-jaje@hotmail.com>
2021-09-20 19:18:52 +0700

    add switch and footer

--- src/components/CardContent.js
+++ src/components/CardContent.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Slider from './Slider';
+import iconCheck from '../images/icon-check.svg';
 
 const CardContent = () => {
   const [price, setPrice] = useState(16);
@@ -18,42 +19,61 @@ const CardContent = () => {
   return (
     <main className="main-content">
       <div className="content">
-        <div className="top">
-          <div>
-            <span>100K Pageviews</span>
+        <div className="content-sub">
+          <div className="top">
+            <div>
+              <span>100K Pageviews</span>
+            </div>
+            <div className="top-right">
+              <h1>${parseFloat(price).toFixed(2)} </h1>
+              <p> / month</p>
+            </div>
           </div>
-          <div className="top-right">
-            <h1>${parseFloat(price).toFixed(2)} </h1>
-            <p> / month</p>
+          <div className="volume">
+            <Slider
+              min="0"
+              max="32"
+              step="1"
+              value={price}
+              onChange={handleVolume}
+              className="volume-slider"
+            />
           </div>
-        </div>
-        <div className="volume">
-          <Slider
-            min="0"
-            max="32"
-            step="1"
-            value={price}
-            onChange={handleVolume}
-          />
+
+          <div className="bill">
+            <div>
+              <span>Monthly Billing</span>
+            </div>
+            <label className="switch">
+              <input type="checkbox" value={bill} onChange={handleSwitch} />
+              <span className="switch-slider green"></span>
+            </label>
+            <div>
+              <span>Yearly Billing</span>
+            </div>
+            <div className="tag">
+              <span>25% discount</span>
+            </div>
+          </div>
         </div>
 
-        <div className="bill">
+        <div className="footer">
           <div>
-            <span>Monthly Billing</span>
+            <p>
+              <img src={iconCheck} alt="" />
+              Unlimited websites
+            </p>
+            <p>
+              <img src={iconCheck} alt="" />
+              100% data ownership
+            </p>
+            <p>
+              <img src={iconCheck} alt="" />
+              Email reports
+            </p>
           </div>
-          <label className="switch">
-            <input type="checkbox" value={bill} onChange={handleSwitch} />
-            <span className="switch-slider green"></span>
-          </label>
-          <div>
-            <span>Yearly Billing</span>
-          </div>
-          <div className="tag">
-            <span>25% discount</span>
-          </div>
+          <button>Start my trial</button>
         </div>
-
-        <footer></footer>
       </div>
     </main>
   );
